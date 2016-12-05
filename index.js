5df2f39c3fabdb94988c2c74e04a23b700c2467f
Sung Park <spark146@jhu.edu>
2016-12-05 03:03:29 -0500

    trying things out

--- index.js
+++ index.js
@@ -38,10 +38,7 @@ app.post('/webhook/', (req, res) => {
         if (event.message && event.message.text) {
             let text = event.message.text;
             if(text.includes('hai!')) {
-                initialMessage(sender,  `Hello ${sender.name}," what are you looking for today? Here are some commands you can ask:\n
-                - "Hai, I'm looking for someone who can teach me Algebra 2"\n
-                - "Hai, I'm looking for soneone who can share a ride to BWI tomorrow at 5:30pm"\n
-                - "-help`);
+                initialMessage(sender,  `Hello ${event.sender.name}," what are you looking for today? Here are some commands you can ask:\n- "Hai, I'm looking for someone who can teach me Algebra 2"\n- "Hai, I'm looking for soneone who can share a ride to BWI tomorrow at 5:30pm"\n- "-help"`);
             } else {
                 sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
             }
