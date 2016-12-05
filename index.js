5b40d77d8f35a198b012c2841497bf374901de14
Sung Park <spark146@jhu.edu>
2016-12-05 02:42:08 -0500

    initial action

--- index.js
+++ index.js
@@ -36,12 +36,35 @@ app.post('/webhook/', (req, res) => {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
-            let text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            let text = event.message.text;
+            if(text.toUpperCase().includes('hai!')) {
+                initialMessage(sender, 'Hello!');
+            } else {
+                sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            }
         }
     }
     res.sendStatus(200)
 })
+
+function initialMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
