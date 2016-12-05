8d61206dd6a16b4730508c17e5b0b7ee1948b35e
Sung Park <spark146@jhu.edu>
2016-12-05 02:52:40 -0500

    action separation

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const request = require('request')
 const app = express()
 const token = process.env.FB_PAGE_ACCESS_TOKEN;
 
+import {initialMessage, sendTextMessage} from './actions';
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -47,43 +49,7 @@ app.post('/webhook/', (req, res) => {
     res.sendStatus(200)
 })
 
-function initialMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
 
 
 
